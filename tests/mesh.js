18be91af91e5019aeec3bece19dec4829df85280
Reece Harris <reeceharris@email.com>
2024-01-30 14:59:30 +0000

    Hack three.js to load from local

--- tests/mesh.js
+++ tests/mesh.js
@@ -5,24 +5,21 @@ const {createCanvas} = require('../canvas');
 const OBJLoader = require('../lib/OBJLoader');
 const MTLLoader = require('../lib/MTLLoader');
 
-global.ProgressEvent = class ProgressEvent {
-    constructor(type, {loaded, total}) {
-        this.type = type;
-        this.loaded = loaded;
-        this.total = total;
-    }
-}
-
 const width = 512, height = 512;
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xFFFFFF);
 
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const backgroundTexture = cubeTextureLoader.load('../assets/jpg/bg.0.jpg');
+// console.log(backgroundTexture)
+scene.background = backgroundTexture;
+
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 camera.position.z = 5;
 
-const light = new THREE.PointLight(0xffffff, 1000)
-light.position.set(2.5, 7.5, 15)
+const light = new THREE.PointLight(0xffffff, 100)
+light.position.set(0, 1, 15)
 scene.add(light)
 
 var ambientLight = new THREE.AmbientLight(0xffffff, 1000); // Color: white, Intensity: 1
@@ -36,7 +33,7 @@ const renderer = new THREE.WebGLRenderer({
 
 const mtlLoader = new MTLLoader();
 mtlLoader.load(
-	'https://raw.githubusercontent.com/NotReeceHarris/open-captcha/main/assets/police.mtl',
+	'./assets/mtl/police.mtl',
 	function ( material ) {
         console.log('Loaded material')
         material.preload();
@@ -44,32 +41,29 @@ mtlLoader.load(
         const objLoader = new OBJLoader();
         objLoader.setMaterials(material);
         objLoader.load(
-            'https://raw.githubusercontent.com/NotReeceHarris/open-captcha/main/assets/police.obj',
+            './assets/obj/police.obj',
             function ( object ) {
                 console.log('Loaded object')
-   
-                //console.log(material)
-                // console.log(object)
+                const mesh = object.children[0]
+                //mesh.material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+                mesh.material = material.materials.Material
+                    
+                // console.log(mesh.material.map)
 
-                //object.children[0].material = material.materials.Material
+                mesh.rotation.y = Math.PI / 2;
+                mesh.rotation.x = Math.PI / 5;
 
-                //console.log(material.materials.Material)
-
-                console.log(object)
-                // rotate object by 90 degrees
-                object.rotation.y = Math.PI / 2;
-                object.rotation.x = Math.PI / 5;
-
-                scene.add( object );
+                scene.add( mesh );
         
                 // export to png
                 renderer.render(scene, camera);
                 const buffer = canvas.toBuffer('image/png');
                 fs.writeFileSync('./test.png', buffer);
+                console.log('Generated image')
         
             },
             function ( xhr ) {
-                console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+                // console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
             },
             function ( error ) {
                 console.log( 'An error happened', error );
@@ -78,7 +72,7 @@ mtlLoader.load(
 
 	},
 	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		// console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 	},
 	function ( error ) {
 		console.log( 'An error happened', error );

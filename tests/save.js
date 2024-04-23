dd46f821dd6b2ee47e92ab5ac43ecfbb7cdac47c
Reece Harris <reeceharris@email.com>
2024-04-23 16:15:35 +0100

    Update README.md with proof of concept information and examples

--- tests/save.js
+++ tests/save.js
@@ -1,5 +1,6 @@
 (async () => {
     const Captcha = require('../dist/src/captcha');
+    const { randomBytes } = require('crypto');
     const fs = require('fs');
     const oCaptcha = new Captcha();
 
@@ -7,8 +8,8 @@
     const generation = await oCaptcha.generate();
     console.timeEnd('Generation')
 
-    if (!fs.existsSync('./out')) {
-        fs.mkdirSync('./out');
+    if (!fs.existsSync('./examples')) {
+        fs.mkdirSync('./examples');
     }
 
     for (let i = 0; i < generation.images.length; i++) {
@@ -16,7 +17,7 @@
 
         console.log(await oCaptcha.verify(image.token, generation.anwser.token))
 
-        fs.writeFileSync(`./out/${i+1}.png`, image.base64.split('base64,').pop(), { encoding: 'base64'}, (err) => {
+        fs.writeFileSync(`./examples/v0_1_x-${randomBytes(2).toString('hex')}.png`, image.base64.split('base64,').pop(), { encoding: 'base64'}, (err) => {
             if (err) {
                 console.error(err);
             }
